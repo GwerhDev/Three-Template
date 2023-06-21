dbe6ba16d13a353ce6c0d5b4a06ec1fa4fd6a1de
Lalo Freak <lalofreak.dev@gmail.com>
2023-06-20 23:00:25 -0400

    basic model and functions

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 // vite.config.js
 module.exports = {
   build: {
+    base: './',
     rollupOptions: {
       // Agrega la configuración de externalización para index.html y main.js
       external: ['index.html', 'src/main.js']
